bf962996710773cf8800f0bcfd65edf8a78b364c
Philippe de Bekker <P.M.deBekker@student.tudelft.nl>
2023-05-30 17:16:57 +0200

    Create script to generate input data per house

--- scripts/toloka_1st_stage_prompt_generator.py
+++ scripts/toloka_1st_stage_prompt_generator.py
@@ -8,10 +8,90 @@ import zipborders
 __DATA_FILE = os.path.join(os.path.dirname(os.path.dirname(
     os.path.abspath(__file__))), "data", "HousesInfo.txt")
 
+__INPUT_DATA_FOLDER = os.path.join(os.path.dirname(os.path.dirname(
+    os.path.abspath(__file__))), "data", "input_data", "1st_stage")
+
 __PROMPT_FOLDER = os.path.join(os.path.dirname(os.path.dirname(
     os.path.abspath(__file__))), "data", "prompts", "1st_stage")
 
 house_description: str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+
+def generate_input_data():
+    input_data = []
+
+    # if __INPUT_DATA_FOLDER does not exist, create it
+    if not os.path.exists(__INPUT_DATA_FOLDER):
+        os.makedirs(__INPUT_DATA_FOLDER)
+
+    with open(__DATA_FILE, "r") as f:
+        house_nr: int = 1
+        for line in f:
+            print('Generating input data for house number: ', house_nr)
+            values: List[str] = line.strip().split()
+
+            # only process lines with 5 columns
+            if len(values) == 5:
+                bedrooms: str = values[0]
+                bathrooms: str = values[1]
+                sq_ft_str: str = values[2]
+                zipcode: str = values[3]
+                price: int = int(values[4])
+
+                lat, long = zip2latlong.convert_zip_to_latlong(zipcode)
+
+                # skip if no latitude/longitude data is available
+                if lat is not None and not math.isnan(lat) and long is not None and not math.isnan(long):
+
+                    # get the bounds of the zip code
+                    bounds = zipborders.get_zipcode_bounds(zipcode)
+
+                    # only process if bounds are available, at least more than 2 points (else it is a point or line)
+                    if bounds is not None and len(bounds) > 2:
+
+                        # create the json for the input data
+                        input = create_json_input_data(
+                            int(house_nr), lat, long, int(bedrooms), float(bathrooms), int(sq_ft_str), zipcode, price, bounds)
+
+                        # save to collection
+                        input_data.append(input)
+
+                        # save input data to .json file in __INPUT_DATA_FOLDER with name <house_nr>.json
+                        with open(os.path.join(__INPUT_DATA_FOLDER, str(house_nr) + ".json"), "w") as f:
+                            json.dump([input], f, indent=2)
+
+            house_nr += 1
+
+    # save all input_data to a single .json file in __INPUT_DATA_FOLDER with name input_data.json
+    with open(os.path.join(__INPUT_DATA_FOLDER, "input_data.json"), "w") as f:
+        json.dump(input_data, f, indent=2)
+
+
+def create_json_input_data(house_nr: int, lat: float, long: float, bedrooms: int, bathrooms: float, sq_ft: int, zipcode: str, price: int, bounds: List[List[float]]):
+
+    # Convert sq_ft to m2 to have both values available
+    m2: float = float(sq_ft) * 0.09290304
+
+    return {
+        "input_values": {
+            "id": house_nr,
+            "price": price,
+            "n_bedrooms": bedrooms,
+            "n_bathrooms": bathrooms,
+            "surface_area_sq_ft": sq_ft,
+            "surface_area_m2": round(m2),  # round for better readability
+            "image_urls": {
+                "frontal_view": "https://raw.githubusercontent.com/emanhamed/Houses-dataset/master/Houses%20Dataset/" + str(house_nr) + "_frontal.jpg",
+                "kitchen": "https://raw.githubusercontent.com/emanhamed/Houses-dataset/master/Houses%20Dataset/" + str(house_nr) + "_kitchen.jpg",
+                "bedroom": "https://raw.githubusercontent.com/emanhamed/Houses-dataset/master/Houses%20Dataset/" + str(house_nr) + "_bedroom.jpg",
+                "bathroom": "https://raw.githubusercontent.com/emanhamed/Houses-dataset/master/Houses%20Dataset/" + str(house_nr) + "_bathroom.jpg"
+            },
+            "coordinates": {
+                "center": str(lat) + "," + str(long),
+                "polygon": bounds
+            }
+        }
+    }
 
 
 def generate_prompts():
